1a8bc0a9a4e13d1192867c85771e6f20d110471a
Sidharth Potdar <sidharth.potdar@gmail.com>
2019-10-27 06:55:44 -0400

    aesthetics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import Main from './pages/Main';
 import UploadPage from './pages/UploadPage';
 import Amplify, { Storage } from 'aws-amplify';
-// import awsconfig from './aws-exports';
+import awsconfig from './aws-exports';
 import './App.css';
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 function App() {
   return (
